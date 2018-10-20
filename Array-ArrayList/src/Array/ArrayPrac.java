1f4d91cf5e206a08a65d6337a2583901ae268d83
Gujral <Gujral@Gujral-PC>
2018-10-20 16:54:40 +0530

    First Implementation

---
+++ Array-ArrayList/src/Array/ArrayPrac.java
@@ -0,0 +1,71 @@
+package Array;
+
+public class ArrayPrac  {
+
+	public static void main(String[] args) {
+		/*int [] arr = new int [5];
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+		
+		for (int k = 0 ; k<= arr.length-1 ; k++){
+			System.out.println(arr[k]);
+		}
+*/
+		
+		
+		/*String [] arrd =  new String[5];
+		arrd[0] = "One";
+		arrd[1] = "two";
+		arrd[2] = "three";
+		arrd[3] = "";
+		arrd[4] = "test";
+		
+		for (int j = 0 ; j<=arrd.length -1 ; j++) {
+			System.out.println(arrd[j]);
+		}
+				
+		
+		System.out.println(arrd[2]);
+		*/
+		
+		
+		int arr[] [] = new int [4] [4];
+		arr [0][0] = 10;
+		arr [0][1] = 20;
+		arr [0][2] = 30;
+		arr [0][3] = 40;
+		
+		arr [1][0] = 5;
+		arr [1][1] = 5;
+		arr [1][2] = 7;
+		arr [1][3] = 7;
+		
+		arr[2][0] =  67;
+		arr[2][1] =  78;
+		arr[2][2] =  90;
+		arr[2][3] =  56;
+		
+		arr[3][0] = 56;
+		arr[3][1] = 8;
+		arr[3][2] = 0;
+		arr[3][3] = 56;
+		
+		
+		for (int i = 0 ; i<= arr.length - 1 ; i++){
+			
+			for (int j = 0 ; j<=arr.length - 1 ; j++){
+				
+			System.out.println(arr[i][j]);	
+				
+			}	
+						
+		}
+		
+		
+		
+	}
+
+}
